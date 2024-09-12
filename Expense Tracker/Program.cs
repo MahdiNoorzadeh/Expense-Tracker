6eafea077a4078d578df3856f2dbafc14ad8c7f6
Mahdi <mahdinoorzadeh1@gmail.com>
2024-09-12 22:19:09 +0330

    Add project files.

---
+++ Expense Tracker/Program.cs
@@ -0,0 +1,43 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF1cWWhAYVJ+WmFZfVpgdVdMZFlbR3dPIiBoS35RckVqWH5fcnBQRWFYUURz\r\n");
+
+
+            // Dependency Injection
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
